831312fb82d207b453ae3a6569cbf3534ed6c0c6
cfrancoi <cfrancoi@z4r5p7.42lyon.fr>
2021-04-12 14:29:47 +0200

    [patch] redirect wp and pma

--- srcs/wordpress/srcs/install_wp.sh
+++ srcs/wordpress/srcs/install_wp.sh
@@ -8,7 +8,7 @@ done
 
 if ! ( wp-cli core is-installed --path=/usr/share/nginx/html/wordpress )
 then
-wp-cli core install --url=https://192.168.99.110:5050 --title=Magie --admin_user=root --admin_password=1234 --admin_email=corentinfrancois31@gmail.com --path=/usr/share/nginx/html/wordpress
+wp-cli core install --url=192.168.99.180:5050 --title=Magie --admin_user=root --admin_password=1234 --admin_email=corentinfrancois31@gmail.com --path=/usr/share/nginx/html/wordpress
 wp-cli option update comment_whitelist 0 --path=/usr/share/nginx/html/wordpress
 wp-cli option update comments_notify 0 --path=/usr/share/nginx/html/wordpress
 wp-cli user create bob bob@example.com --user_pass=123 --role=author --path=/usr/share/nginx/html/wordpress
